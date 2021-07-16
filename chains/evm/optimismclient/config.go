2ea12e299aff584913f810d73e41949b9ba785dc
vezenovm <mvezenov@gmail.com>
2021-07-16 10:06:18 -0400

    initial client changes

---
+++ chains/evm/optimismclient/config.go
@@ -0,0 +1,95 @@
+package optimismclient
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/ChainSafe/chainbridge-core/config"
+	"github.com/ChainSafe/chainbridge-core/crypto/secp256k1"
+	"github.com/spf13/viper"
+)
+
+const DefaultGasLimit = 6721975
+const DefaultGasPrice = 15000000
+const DefaultGasMultiplier = 1
+const DefaultBlockConfirmations = 10
+
+type OptimismConfig struct {
+	SharedEVMConfig config.SharedEVMConfig
+	kp              *secp256k1.Keypair
+	rollupEndpoint  string // This is the endpoint for an Optimism verifier node
+}
+
+type RawOptimismConfig struct {
+	config.RawSharedEVMConfig `mapstructure:",squash"`
+	rollupEndpoint            string `mapstructure:"rollupEndpoint"`
+}
+
+func NewConfig() *OptimismConfig {
+	return &OptimismConfig{}
+}
+
+func GetConfig(path string, name string) (*RawOptimismConfig, error) {
+	config := &RawOptimismConfig{}
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName(name)
+	viper.SetConfigType("json")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read in the config file, error: %w", err)
+	}
+
+	if err := viper.Unmarshal(config); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config into struct, error: %w", err)
+	}
+
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func ParseConfig(rawConfig *RawOptimismConfig) (*OptimismConfig, error) {
+
+	cfg, err := rawConfig.RawSharedEVMConfig.ParseConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse shared evm config, error: %w", err)
+	}
+
+	config := &OptimismConfig{
+		SharedEVMConfig: *cfg,
+		rollupEndpoint:  rawConfig.rollupEndpoint,
+	}
+
+	return config, nil
+}
+
+func (c *RawOptimismConfig) ToJSON(file string) *os.File {
+	var (
+		newFile *os.File
+		err     error
+	)
+
+	var raw []byte
+	if raw, err = json.Marshal(&c); err != nil {
+		fmt.Println("error marshalling json", "err", err)
+		os.Exit(1)
+	}
+
+	newFile, err = os.Create(file)
+	if err != nil {
+		fmt.Println("error creating config file", "err", err)
+	}
+	_, err = newFile.Write(raw)
+	if err != nil {
+		fmt.Println("error writing to config file", "err", err)
+	}
+
+	if err := newFile.Close(); err != nil {
+		fmt.Println("failed to unmarshal config into struct", "err", err)
+	}
+	return newFile
+}
---
+++ chains/evm/optimismclient/optimism-client.go
@@ -0,0 +1,463 @@
+package optimismclient
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/big"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/listener"
+	"github.com/ChainSafe/chainbridge-core/crypto/secp256k1"
+	"github.com/ChainSafe/chainbridge-core/keystore"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/go-resty/resty/v2"
+	"github.com/rs/zerolog/log"
+)
+
+// Batch represents the data structure that is submitted with
+// a series of transactions to layer one
+type Batch struct {
+	Index             uint64         `json:"index"`
+	Root              common.Hash    `json:"root,omitempty"`
+	Size              uint32         `json:"size,omitempty"`
+	PrevTotalElements uint32         `json:"prevTotalElements,omitempty"`
+	ExtraData         hexutil.Bytes  `json:"extraData,omitempty"`
+	BlockNumber       uint64         `json:"blockNumber"`
+	Timestamp         uint64         `json:"timestamp"`
+	Submitter         common.Address `json:"submitter"`
+}
+
+// transaction represents the return result of the remote server.
+// It either came from a batch or was replicated from the sequencer.
+type transaction struct {
+	Index       uint64          `json:"index"`
+	BatchIndex  uint64          `json:"batchIndex"`
+	BlockNumber uint64          `json:"blockNumber"`
+	Timestamp   uint64          `json:"timestamp"`
+	Value       *hexutil.Big    `json:"value"`
+	GasLimit    uint64          `json:"gasLimit,string"`
+	Target      common.Address  `json:"target"`
+	Origin      *common.Address `json:"origin"`
+	Data        hexutil.Bytes   `json:"data"`
+	QueueOrigin string          `json:"queueOrigin"`
+	QueueIndex  *uint64         `json:"queueIndex"`
+	Decoded     *decoded        `json:"decoded"`
+}
+
+// signature represents a secp256k1 ECDSA signature
+type signature struct {
+	R hexutil.Bytes `json:"r"`
+	S hexutil.Bytes `json:"s"`
+	V uint          `json:"v"`
+}
+
+// decoded represents the decoded transaction from the batch.
+// When this struct exists in other structs and is set to `nil`,
+// it means that the decoding failed.
+type decoded struct {
+	Signature signature       `json:"sig"`
+	Value     *hexutil.Big    `json:"value"`
+	GasLimit  uint64          `json:"gasLimit,string"`
+	GasPrice  uint64          `json:"gasPrice,string"`
+	Nonce     uint64          `json:"nonce,string"`
+	Target    *common.Address `json:"target"`
+	Data      hexutil.Bytes   `json:"data"`
+}
+
+// TransactionBatchResponse represents the response from the remote server
+// when querying batches.
+type TransactionBatchResponse struct {
+	Batch        *Batch         `json:"batch"`
+	Transactions []*transaction `json:"transactions"`
+}
+
+// SyncStatus represents the state of the remote server. The SyncService
+// does not want to begin syncing until the remote server has fully synced.
+type SyncStatus struct {
+	Syncing                      bool   `json:"syncing"`
+	HighestKnownTransactionIndex uint64 `json:"highestKnownTransactionIndex"`
+	CurrentTransactionIndex      uint64 `json:"currentTransactionIndex"`
+}
+
+type OptimismClient struct {
+	*ethclient.Client
+	rpClient     *rpc.Client
+	nonceLock    sync.Mutex
+	config       *OptimismConfig
+	nonce        *big.Int
+	rollupClient *resty.Client
+}
+
+type CommonTransaction interface {
+	// Hash returns the transaction hash.
+	Hash() common.Hash
+	// Returns signed transaction by provided private key
+	RawWithSignature(key *ecdsa.PrivateKey, chainID *big.Int) ([]byte, error)
+}
+
+func NewEVMClient() *OptimismClient {
+	return &OptimismClient{}
+}
+
+func (c *OptimismClient) Configurate(path string, name string) error {
+	rawCfg, err := GetConfig(path, name)
+	if err != nil {
+		return err
+	}
+	cfg, err := ParseConfig(rawCfg)
+	if err != nil {
+		return err
+	}
+	c.config = cfg
+	generalConfig := cfg.SharedEVMConfig.GeneralChainConfig
+
+	kp, err := keystore.KeypairFromAddress(generalConfig.From, keystore.EthChain, generalConfig.KeystorePath, generalConfig.Insecure)
+	if err != nil {
+		panic(err)
+	}
+	krp := kp.(*secp256k1.Keypair)
+	c.config.kp = krp
+
+	log.Info().Str("url", generalConfig.Endpoint).Msg("Connecting to evm chain...")
+	rpcClient, err := rpc.DialContext(context.TODO(), generalConfig.Endpoint)
+	if err != nil {
+		return err
+	}
+	c.Client = ethclient.NewClient(rpcClient)
+	c.rpClient = rpcClient
+	c.rollupClient = c.NewRollupClient(c.config.rollupEndpoint)
+
+	// Wait until the remote service is done syncing
+	tStatus := time.NewTicker(10 * time.Second)
+	for ; true; <-tStatus.C {
+		status, err := c.SyncStatus()
+		if err != nil {
+			log.Error().Msg("Cannot get sync status")
+			continue
+		}
+		if !status.Syncing {
+			tStatus.Stop()
+			break
+		}
+		log.Info().Msgf("Still syncing", "index", status.CurrentTransactionIndex, "tip", status.HighestKnownTransactionIndex)
+	}
+
+	if generalConfig.LatestBlock {
+		curr, err := c.LatestBlock()
+		if err != nil {
+			return err
+		}
+		cfg.SharedEVMConfig.StartBlock = curr
+	}
+
+	return nil
+
+}
+
+func (c *OptimismClient) NewRollupClient(endpoint string) *resty.Client {
+	client := resty.New()
+	client.SetHostURL(endpoint)
+	client.SetHeader("User-Agent", "verifier")
+	client.OnAfterResponse(func(c *resty.Client, r *resty.Response) error {
+		statusCode := r.StatusCode()
+		if statusCode >= 400 {
+			method := r.Request.Method
+			url := r.Request.URL
+			return fmt.Errorf("%d cannot %s %s: %v", statusCode, method, url, "http error")
+		}
+		return nil
+	})
+	return client
+}
+
+// GetLatestTransactionBatchIndex returns the latest transaction batch index
+func (c *OptimismClient) GetLatestTransactionBatchIndex() (*uint64, error) {
+	batch, err := c.GetLatestTransactionBatch()
+	if err != nil {
+		return nil, err
+	}
+	index := batch.Index
+	return &index, nil
+}
+
+// GetLatestTransactionBatch will return the latest transaction batch
+func (c *OptimismClient) GetLatestTransactionBatch() (*Batch, error) {
+	response, err := c.rollupClient.R().
+		SetResult(&TransactionBatchResponse{}).
+		Get("/batch/transaction/latest")
+
+	if err != nil {
+		return nil, errors.New("Cannot get latest transaction batch")
+	}
+	txBatch, ok := response.Result().(*TransactionBatchResponse)
+	if !ok {
+		return nil, fmt.Errorf("Cannot parse transaction batch response")
+	}
+	return txBatch.Batch, nil
+}
+
+// SyncStatus will query the remote server to determine if it is still syncing
+func (c *OptimismClient) SyncStatus() (*SyncStatus, error) {
+	response, err := c.rollupClient.R().
+		SetResult(&SyncStatus{}).
+		SetQueryParams(map[string]string{
+			"backend": "l1", // We are only concerned with transactions that have been batched to Layer One
+		}).
+		Get("/eth/syncing")
+
+	if err != nil {
+		return nil, fmt.Errorf("Cannot fetch sync status: %w", err)
+	}
+
+	status, ok := response.Result().(*SyncStatus)
+	if !ok {
+		return nil, fmt.Errorf("Cannot parse sync status")
+	}
+
+	return status, nil
+}
+
+func (c *OptimismClient) RollupIsVerified() (bool, error) {
+	status, err := c.SyncStatus()
+	if err != nil {
+		return false, err
+	}
+
+	if !status.Syncing {
+		return false, nil
+	}
+	index, err := c.GetLatestTransactionBatchIndex()
+	if *index <= status.CurrentTransactionIndex {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+type headerNumber struct {
+	Number *big.Int `json:"number"           gencodec:"required"`
+}
+
+func (h *headerNumber) UnmarshalJSON(input []byte) error {
+	type headerNumber struct {
+		Number *hexutil.Big `json:"number" gencodec:"required"`
+	}
+	var dec headerNumber
+	if err := json.Unmarshal(input, &dec); err != nil {
+		return err
+	}
+	if dec.Number == nil {
+		return errors.New("missing required field 'number' for Header")
+	}
+	h.Number = (*big.Int)(dec.Number)
+	return nil
+}
+
+// LatestBlock returns the latest block from the current chain
+func (c *OptimismClient) LatestBlock() (*big.Int, error) {
+	var head *headerNumber
+
+	err := c.rpClient.CallContext(context.Background(), &head, "eth_getBlockByNumber", toBlockNumArg(nil), false)
+	if err == nil && head == nil {
+		err = ethereum.NotFound
+	}
+	return head.Number, err
+}
+
+const (
+	DepositSignature string = "Deposit(uint8,bytes32,uint64)"
+)
+
+func (c *OptimismClient) FetchDepositLogs(ctx context.Context, contractAddress common.Address, startBlock *big.Int, endBlock *big.Int) ([]*listener.DepositLogs, error) {
+	definition := "[{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint8\",\"name\":\"destinationChainID\",\"type\":\"uint8\"},{\"indexed\":false,\"internalType\":\"bytes32\",\"name\":\"resourceID\",\"type\":\"bytes32\"},{\"indexed\":false,\"internalType\":\"uint64\",\"name\":\"depositNonce\",\"type\":\"uint64\"}],\"name\":\"Deposit\",\"type\":\"event\"}]"
+	contractAbi, err := abi.JSON(strings.NewReader(definition))
+	if err != nil {
+		log.Fatal().Msgf("error: %v", err)
+	}
+
+	logs, err := c.FilterLogs(ctx, buildQuery(contractAddress, DepositSignature, startBlock, endBlock))
+	if err != nil {
+		return nil, err
+	}
+	depositLogs := make([]*listener.DepositLogs, 0)
+	for _, l := range logs {
+		var dl listener.DepositLogs
+		err := contractAbi.UnpackIntoInterface(&dl, "Deposit", l.Data)
+		if err != nil {
+			log.Fatal().Msgf("error: %v", err)
+		}
+		log.Info().Msgf("Deposit Logs dest chain id: %v, deposit nonce: %v, resource id: %v", dl.DestinationChainID, dl.DepositNonce, dl.ResourceID)
+		depositLogs = append(depositLogs, &dl)
+	}
+
+	return depositLogs, nil
+}
+
+// SendRawTransaction accepts rlp-encode of signed transaction and sends it via RPC call
+func (c *OptimismClient) SendRawTransaction(ctx context.Context, tx []byte) error {
+	return c.rpClient.CallContext(ctx, nil, "eth_sendRawTransaction", hexutil.Encode(tx))
+}
+
+func (c *OptimismClient) CallContract(ctx context.Context, callArgs map[string]interface{}, blockNumber *big.Int) ([]byte, error) {
+	var hex hexutil.Bytes
+	err := c.rpClient.CallContext(ctx, &hex, "eth_call", callArgs, toBlockNumArg(blockNumber))
+	if err != nil {
+		return nil, err
+	}
+	return hex, nil
+}
+
+func (c *OptimismClient) PendingCallContract(ctx context.Context, callArgs map[string]interface{}) ([]byte, error) {
+	var hex hexutil.Bytes
+	err := c.rpClient.CallContext(ctx, &hex, "eth_call", callArgs, "pending")
+	if err != nil {
+		return nil, err
+	}
+	return hex, nil
+}
+
+//func (c *EVMClient) ChainID()
+
+func (c *OptimismClient) SignAndSendTransaction(ctx context.Context, tx CommonTransaction) (common.Hash, error) {
+	id, err := c.ChainID(ctx)
+	if err != nil {
+		panic(err)
+	}
+	rawTX, err := tx.RawWithSignature(c.config.kp.PrivateKey(), id)
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	err = c.SendRawTransaction(ctx, rawTX)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return tx.Hash(), nil
+}
+
+func (c *OptimismClient) RelayerAddress() common.Address {
+	return c.config.kp.CommonAddress()
+}
+
+func (c *OptimismClient) LockNonce() {
+	c.nonceLock.Lock()
+}
+
+func (c *OptimismClient) UnlockNonce() {
+	c.nonceLock.Unlock()
+}
+
+func (c *OptimismClient) UnsafeNonce() (*big.Int, error) {
+	var err error
+	for i := 0; i <= 10; i++ {
+		if c.nonce == nil {
+			nonce, err := c.PendingNonceAt(context.Background(), c.config.kp.CommonAddress())
+			if err != nil {
+				time.Sleep(1)
+				continue
+			}
+			c.nonce = big.NewInt(0).SetUint64(nonce)
+			return c.nonce, nil
+		}
+		return c.nonce, nil
+	}
+	return nil, err
+}
+
+func (c *OptimismClient) UnsafeIncreaseNonce() error {
+	nonce, err := c.UnsafeNonce()
+	log.Debug().Str("nonce", nonce.String()).Msg("Before increase")
+	if err != nil {
+		return err
+	}
+	c.nonce = nonce.Add(nonce, big.NewInt(1))
+	log.Debug().Str("nonce", c.nonce.String()).Msg("After increase")
+	return nil
+}
+
+func (c *OptimismClient) GasLimit(msg ethereum.CallMsg) *big.Int {
+	if c.config.SharedEVMConfig.GeneralChainConfig.Name == "optimism" {
+		gas, err := c.EstimateGas(context.TODO(), msg)
+		if err != nil {
+			log.Fatal().Msgf("could not estimate gas when transacting with optimism: %v", err)
+		}
+		return big.NewInt(int64(gas))
+	}
+
+	return c.config.SharedEVMConfig.GasLimit
+}
+
+func (c *OptimismClient) GasPrice() (*big.Int, error) {
+	if c.config.SharedEVMConfig.GeneralChainConfig.Name == "optimism" {
+		return big.NewInt(15000000), nil
+	}
+
+	gasPrice, err := c.SafeEstimateGas(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+	return gasPrice, nil
+}
+
+func (c *OptimismClient) SafeEstimateGas(ctx context.Context) (*big.Int, error) {
+	suggestedGasPrice, err := c.SuggestGasPrice(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	gasPrice := multiplyGasPrice(suggestedGasPrice, c.config.SharedEVMConfig.GasMultiplier)
+
+	if gasPrice.Cmp(c.config.SharedEVMConfig.MaxGasPrice) == 1 {
+		return c.config.SharedEVMConfig.MaxGasPrice, nil
+	} else {
+		return gasPrice, nil
+	}
+}
+
+func multiplyGasPrice(gasEstimate *big.Int, gasMultiplier *big.Float) *big.Int {
+
+	gasEstimateFloat := new(big.Float).SetInt(gasEstimate)
+
+	result := gasEstimateFloat.Mul(gasEstimateFloat, gasMultiplier)
+
+	gasPrice := new(big.Int)
+
+	result.Int(gasPrice)
+
+	return gasPrice
+}
+
+func toBlockNumArg(number *big.Int) string {
+	if number == nil {
+		return "latest"
+	}
+	return hexutil.EncodeBig(number)
+}
+
+// buildQuery constructs a query for the bridgeContract by hashing sig to get the event topic
+func buildQuery(contract common.Address, sig string, startBlock *big.Int, endBlock *big.Int) ethereum.FilterQuery {
+	query := ethereum.FilterQuery{
+		FromBlock: startBlock,
+		ToBlock:   endBlock,
+		Addresses: []common.Address{contract},
+		Topics: [][]common.Hash{
+			{crypto.Keccak256Hash([]byte(sig))},
+		},
+	}
+	return query
+}
+
+func (c *OptimismClient) GetConfig() *OptimismConfig {
+	return c.config
+}
