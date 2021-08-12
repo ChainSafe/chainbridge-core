aee20f1f27cafefe148b0c51d776b6abb7f554f3
waymobetta <jbroethke@gmail.com>
2021-08-12 10:02:26 -0700

    Fix for subcommand consistency + descriptions

--- chains/evm/cli/account/account.go
+++ chains/evm/cli/account/account.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"bytes"
+
 	"github.com/ChainSafe/chainbridge-core/crypto/secp256k1"
 	accountutils "github.com/ChainSafe/chainbridge-core/keystore/account"
 	"github.com/rs/zerolog/log"
@@ -21,19 +22,18 @@ func init() {
 }
 
 var importPrivKeyCmd = &cobra.Command{
-	Use:   "import-pk",
-	Short: "Add a minter to an Erc20 mintable contract",
-	Long:  "Add a minter to an Erc20 mintable contract",
+	Use:   "import",
+	Short: "Import bridge keystore",
+	Long:  "The import subcommand is used to import a keystore for the bridge.",
 	RunE:  importPrivKey,
 }
 
 var generateKeyPairCmd = &cobra.Command{
-	Use:   "generate-keypair",
-	Short: "Add a minter to an Erc20 mintable contract",
-	Long:  "Add a minter to an Erc20 mintable contract",
+	Use:   "generate",
+	Short: "Generate bridge keystore (Secp256k1)",
+	Long:  "The generate subcommand is used to generate the bridge keystore. If no options are specified, a secp256k1 key will be made.",
 	RunE:  generateKeyPair,
 }
-
 
 func importPrivKey(cmd *cobra.Command, args []string) error {
 	pk, err := cmd.Flags().GetString("privateKey")
