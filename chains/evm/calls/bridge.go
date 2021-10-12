78887c84e12efc43a2cfb83cd00350a18911e332
Nikola Mlinarić <35880252+nmlinaric@users.noreply.github.com>
2021-10-12 09:08:06 +0200

    Define resource ID type (#117)
    * Define type for resource ID
    
    * Use resource ID as type instead of [32]bytes + rename rID to resourceID
    
    * Replace [32]byte with resource id type

--- chains/evm/calls/bridge.go
+++ chains/evm/calls/bridge.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/ChainSafe/chainbridge-core/chains/evm/voter/proposal"
+	"github.com/ChainSafe/chainbridge-core/types"
 
 	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/consts"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/voter/proposal"
