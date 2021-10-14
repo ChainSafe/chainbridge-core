91a1c7424140df79b1571cf5f99f4a5ce6f97844
Matija Petrunic <matija.petrunic@fer.hr>
2021-10-14 13:47:53 +0200

    Add generic handler unit tests

---
+++ chains/evm/listener/event-handler_test.go
@@ -0,0 +1,218 @@
+package listener_test
+
+import (
+	"errors"
+	"math/big"
+	"testing"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/listener"
+	"github.com/ChainSafe/chainbridge-core/relayer"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/stretchr/testify/suite"
+)
+
+var errIncorrectCalldataLen = errors.New("invalid calldata length: less than 84 bytes")
+
+type Erc20HandlerTestSuite struct {
+	suite.Suite
+}
+
+func TestRunErc20HandlerTestSuite(t *testing.T) {
+	suite.Run(t, new(Erc20HandlerTestSuite))
+}
+
+func (s *Erc20HandlerTestSuite) SetupSuite()    {}
+func (s *Erc20HandlerTestSuite) TearDownSuite() {}
+func (s *Erc20HandlerTestSuite) SetupTest()     {}
+func (s *Erc20HandlerTestSuite) TearDownTest()  {}
+
+func (s *Erc20HandlerTestSuite) TestErc20HandleEvent() {
+	// 0xf1e58fb17704c2da8479a533f9fad4ad0993ca6b
+	recipientByteSlice := []byte{241, 229, 143, 177, 119, 4, 194, 218, 132, 121, 165, 51, 249, 250, 212, 173, 9, 147, 202, 107}
+
+	// construct ERC20 deposit data
+	// follows behavior of solidity tests
+	// https://github.com/ChainSafe/chainbridge-solidity/blob/develop/test/contractBridge/depositERC20.js#L46-L50
+	var calldata []byte
+	calldata = append(calldata, math.PaddedBigBytes(big.NewInt(2), 32)...)
+	calldata = append(calldata, math.PaddedBigBytes(big.NewInt(int64(len(recipientByteSlice))), 32)...)
+	calldata = append(calldata, recipientByteSlice...)
+
+	depositLog := &listener.DepositLogs{
+		DestinationID:   0,
+		ResourceID:      [32]byte{0},
+		DepositNonce:    1,
+		SenderAddress:   common.HexToAddress("0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485"),
+		Calldata:        calldata,
+		HandlerResponse: []byte{},
+	}
+
+	sourceID := uint8(1)
+	amountParsed := calldata[:32]
+	recipientAddressParsed := calldata[64:]
+
+	expected := &relayer.Message{
+		Source:       sourceID,
+		Destination:  depositLog.DestinationID,
+		DepositNonce: depositLog.DepositNonce,
+		ResourceId:   depositLog.ResourceID,
+		Type:         relayer.FungibleTransfer,
+		Payload: []interface{}{
+			amountParsed,
+			recipientAddressParsed,
+		},
+	}
+
+	message, err := listener.Erc20EventHandler(sourceID, depositLog.DestinationID, depositLog.DepositNonce, depositLog.ResourceID, depositLog.Calldata, depositLog.HandlerResponse)
+
+	s.Nil(err)
+	s.NotNil(message)
+	s.Equal(message, expected)
+}
+
+func (s *Erc20HandlerTestSuite) TestErc20HandleEventIncorrectCalldataLen() {
+	metadata := []byte("0xdeadbeef")
+
+	var calldata []byte
+	calldata = append(calldata, math.PaddedBigBytes(big.NewInt(int64(len(metadata))), 32)...)
+	calldata = append(calldata, metadata...)
+
+	depositLog := &listener.DepositLogs{
+		DestinationID:   0,
+		ResourceID:      [32]byte{0},
+		DepositNonce:    1,
+		SenderAddress:   common.HexToAddress("0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485"),
+		Calldata:        calldata,
+		HandlerResponse: []byte{},
+	}
+
+	sourceID := uint8(1)
+
+	message, err := listener.Erc20EventHandler(sourceID, depositLog.DestinationID, depositLog.DepositNonce, depositLog.ResourceID, depositLog.Calldata, depositLog.HandlerResponse)
+
+	s.Nil(message)
+	s.EqualError(err, errIncorrectCalldataLen.Error())
+}
+
+type GenericHandlerTestSuite struct {
+	suite.Suite
+}
+
+func TestRunGenericHandlerTestSuite(t *testing.T) {
+	suite.Run(t, new(GenericHandlerTestSuite))
+}
+
+func (s *GenericHandlerTestSuite) SetupSuite()    {}
+func (s *GenericHandlerTestSuite) TearDownSuite() {}
+func (s *GenericHandlerTestSuite) SetupTest()     {}
+func (s *GenericHandlerTestSuite) TearDownTest()  {}
+
+func (s *GenericHandlerTestSuite) TestGenericHandleEventIncorrectCalldataLen() {
+	metadata := []byte("0xdeadbeef")
+
+	var calldata []byte
+	calldata = append(calldata, math.PaddedBigBytes(big.NewInt(int64(len(metadata))), 16)...)
+	calldata = append(calldata, metadata...)
+
+	depositLog := &listener.DepositLogs{
+		DestinationID:   0,
+		ResourceID:      [32]byte{0},
+		DepositNonce:    1,
+		SenderAddress:   common.HexToAddress("0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485"),
+		Calldata:        calldata,
+		HandlerResponse: []byte{},
+	}
+
+	sourceID := uint8(1)
+	message, err := listener.GenericEventHandler(
+		sourceID,
+		depositLog.DestinationID,
+		depositLog.DepositNonce,
+		depositLog.ResourceID,
+		depositLog.Calldata,
+		depositLog.HandlerResponse,
+	)
+
+	s.Nil(message)
+	s.EqualError(err, "invalid calldata length: less than 32 bytes")
+}
+
+func (s *GenericHandlerTestSuite) TestGenericHandleEventEmptyMetadata() {
+	metadata := []byte("")
+	calldata := calls.ConstructGenericDepositData(metadata)
+
+	depositLog := &listener.DepositLogs{
+		DestinationID:   0,
+		ResourceID:      [32]byte{0},
+		DepositNonce:    1,
+		SenderAddress:   common.HexToAddress("0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485"),
+		Calldata:        calldata,
+		HandlerResponse: []byte{},
+	}
+
+	sourceID := uint8(1)
+	expected := &relayer.Message{
+		Source:       sourceID,
+		Destination:  depositLog.DestinationID,
+		DepositNonce: depositLog.DepositNonce,
+		ResourceId:   depositLog.ResourceID,
+		Type:         relayer.GenericTransfer,
+		Payload: []interface{}{
+			metadata,
+		},
+	}
+
+	message, err := listener.GenericEventHandler(
+		sourceID,
+		depositLog.DestinationID,
+		depositLog.DepositNonce,
+		depositLog.ResourceID,
+		depositLog.Calldata,
+		depositLog.HandlerResponse,
+	)
+
+	s.Nil(err)
+	s.NotNil(message)
+	s.Equal(message, expected)
+}
+
+func (s *GenericHandlerTestSuite) TestGenericHandleEvent() {
+	metadata := []byte("0xdeadbeef")
+	calldata := calls.ConstructGenericDepositData(metadata)
+
+	depositLog := &listener.DepositLogs{
+		DestinationID:   0,
+		ResourceID:      [32]byte{0},
+		DepositNonce:    1,
+		SenderAddress:   common.HexToAddress("0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485"),
+		Calldata:        calldata,
+		HandlerResponse: []byte{},
+	}
+
+	sourceID := uint8(1)
+	expected := &relayer.Message{
+		Source:       sourceID,
+		Destination:  depositLog.DestinationID,
+		DepositNonce: depositLog.DepositNonce,
+		ResourceId:   depositLog.ResourceID,
+		Type:         relayer.GenericTransfer,
+		Payload: []interface{}{
+			metadata,
+		},
+	}
+
+	message, err := listener.GenericEventHandler(
+		sourceID,
+		depositLog.DestinationID,
+		depositLog.DepositNonce,
+		depositLog.ResourceID,
+		depositLog.Calldata,
+		depositLog.HandlerResponse,
+	)
+
+	s.Nil(err)
+	s.NotNil(message)
+	s.Equal(message, expected)
+}
