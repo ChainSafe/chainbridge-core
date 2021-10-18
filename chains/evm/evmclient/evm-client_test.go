48d3050efdd77d11f45b416411a3b4e583f419bb
Matija Petrunic <matija.petrunic@fer.hr>
2021-10-18 16:54:40 +0200

    Add unpack deposit event unit tests

--- chains/evm/evmclient/evm-client_test.go
+++ chains/evm/evmclient/evm-client_test.go
@@ -47,7 +47,6 @@ func (s *EvmClientTestSuite) TestUnpackDepositEventLogFailedUnpack() {
 func (s *EvmClientTestSuite) TestUnpackDepositEventLogValidData() {
 	abi, _ := abi.JSON(strings.NewReader(consts.BridgeABI))
 	logDataBytes, _ := hex.DecodeString(logData)
-
 	expectedRID := types.ResourceID(types.ResourceID{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xd6, 0x6, 0xa0, 0xc, 0x1a, 0x39, 0xda, 0x53, 0xea, 0x7b, 0xb3, 0xab, 0x57, 0xb, 0xbe, 0x40, 0xb1, 0x56, 0xeb, 0x66, 0x0})
 
 	dl, err := s.client.UnpackDepositEventLog(abi, logDataBytes)
@@ -57,4 +56,5 @@ func (s *EvmClientTestSuite) TestUnpackDepositEventLogValidData() {
 	s.Equal(dl.DepositNonce, uint64(1))
 	s.Equal(dl.DestinationDomainID, uint8(2))
 	s.Equal(dl.ResourceID, expectedRID)
+	s.Equal(dl.HandlerResponse, []byte{})
 }
