b6a3ba8c676abbc3bac585582e60fcd0c1aff107
waymobetta <jbroethke@gmail.com>
2021-10-27 13:21:53 -0700

    fix misspelling

--- chains/evm/deploy.go
+++ chains/evm/deploy.go
@@ -35,9 +35,9 @@ type E2EClient interface {
 	calls.ClientDeployer
 }
 
-func PrepareEVME2EEnv(ethClient E2EClient, fabric calls.TxFabric, domainID uint8, treshHold *big.Int, mintTo common.Address) (common.Address, common.Address, common.Address, error) {
+func PrepareEVME2EEnv(ethClient E2EClient, fabric calls.TxFabric, domainID uint8, threshold *big.Int, mintTo common.Address) (common.Address, common.Address, common.Address, error) {
 	staticGasPricer := evmgaspricer.NewStaticGasPriceDeterminant(ethClient, nil)
-	bridgeAddr, erc20Addr, erc20HandlerAddr, err := deployForTest(ethClient, fabric, staticGasPricer, domainID, treshHold)
+	bridgeAddr, erc20Addr, erc20HandlerAddr, err := deployForTest(ethClient, fabric, staticGasPricer, domainID, threshold)
 	if err != nil {
 		return common.Address{}, common.Address{}, common.Address{}, err
 	}
@@ -95,13 +95,13 @@ func PrepareEVME2EEnv(ethClient E2EClient, fabric calls.TxFabric, domainID uint8
 	return bridgeAddr, erc20Addr, erc20HandlerAddr, nil
 }
 
-func deployForTest(c E2EClient, fabric calls.TxFabric, gasPriceClient calls.GasPricer, domainID uint8, treshHold *big.Int) (common.Address, common.Address, common.Address, error) {
+func deployForTest(c E2EClient, fabric calls.TxFabric, gasPriceClient calls.GasPricer, domainID uint8, threshold *big.Int) (common.Address, common.Address, common.Address, error) {
 	erc20Addr, err := calls.DeployErc20(c, fabric, gasPriceClient, "Test", "TST")
 	if err != nil {
 		return common.Address{}, common.Address{}, common.Address{}, fmt.Errorf("ERC20 deploy failed: %w", err)
 	}
 
-	bridgeAdrr, err := calls.DeployBridge(c, fabric, gasPriceClient, domainID, DefaultRelayerAddresses, treshHold)
+	bridgeAdrr, err := calls.DeployBridge(c, fabric, gasPriceClient, domainID, DefaultRelayerAddresses, threshold)
 	if err != nil {
 		return common.Address{}, common.Address{}, common.Address{}, fmt.Errorf("bridge deploy failed: %w", err)
 	}
