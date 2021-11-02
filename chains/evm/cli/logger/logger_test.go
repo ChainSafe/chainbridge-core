3b27177b24e8f07501bb44bc2cf01f0b39db5d7f
Nikola <nikola.mlinaric93@gmail.com>
2021-11-02 14:37:05 +0100

    Add test for logger

---
+++ chains/evm/cli/logger/logger_test.go
@@ -0,0 +1,54 @@
+package logger_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/cli"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/cli/logger"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/suite"
+)
+
+type LoggerTestSuite struct {
+	suite.Suite
+	EvmRootCLI *cobra.Command
+}
+
+func TestLoggerWriteToFile(t *testing.T) {
+	suite.Run(t, new(LoggerTestSuite))
+}
+
+func (s *LoggerTestSuite) SetupSuite() {
+}
+func (s *LoggerTestSuite) TearDownSuite() {}
+
+func (s *LoggerTestSuite) TearDownTest() {}
+
+func (s *LoggerTestSuite) TestWriteCliDataToFile() {
+	expectedLog := time.Now().Format("02-01|15:00:00.000 ") +
+		"Called evm-cli with args: --gasLimit=\"7000000\" --gasPrice=\"25000000000\" --help=\"false\" --jsonWallet=\"test-wallet\" --jsonWalletPassword=\"test-wallet-password\" --networkid=\"0\" --url=\"test-url\" =>\n"
+
+	rootCmdArgs := []string{
+		"--url", "test-url",
+		"--gasLimit", "7000000",
+		"--gasPrice", "25000000000",
+		"--networkid", "0x0",
+		"--privateKey", "test-private-key",
+		"--jsonWallet", "test-wallet",
+		"--jsonWalletPassword", "test-wallet-password",
+	}
+
+	cli.EvmRootCLI.SetArgs(rootCmdArgs)
+	cli.EvmRootCLI.Execute()
+
+	data, _ := os.ReadFile(logger.CliLogsFilename)
+	s.Equal(expectedLog, string(data))
+
+	err := os.Remove(logger.CliLogsFilename)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+}
