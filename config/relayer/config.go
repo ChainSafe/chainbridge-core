bcb0977d4502b72e865c78acdc5c7513ead3a225
Matija Petrunic <matija.petrunic@fer.hr>
2021-11-03 12:18:59 +0100

    Implement relayer config

---
+++ config/relayer/config.go
@@ -0,0 +1,36 @@
+package relayer
+
+import (
+	"github.com/spf13/viper"
+)
+
+type RelayerConfig struct {
+	PrometheusEndpoint string
+	PrometheusPort     uint8
+}
+
+func (c *RelayerConfig) Validate() error {
+	return nil
+}
+
+func GetRelayerConfig(path string) (RelayerConfig, error) {
+	config := RelayerConfig{}
+
+	viper.SetConfigFile(path)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return config, err
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, err
+	}
+
+	err = config.Validate()
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
