948fb00cf1681dfd344232fbeb77864f333df429
Matija Petrunic <matija.petrunic@fer.hr>
2021-11-04 11:11:10 +0100

    Move message definition to separate package to prevent import cycle

--- relayer/message/message_processors.go
+++ relayer/message/message_processors.go
@@ -1,4 +1,4 @@
-package relayer
+package message
 
 import (
 	"errors"
--- relayer/message/message_test.go
+++ relayer/message/message_test.go
@@ -1,7 +1,7 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
 
-package relayer
+package message
 
 import (
 	"math/big"
