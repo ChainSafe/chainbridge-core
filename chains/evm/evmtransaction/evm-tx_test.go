7f349914e93be0f8d61e7be79bd3911d9bd0b5e6
Kirill Pisarev <pisarevkir@gmail.com>
2021-11-05 13:13:29 +0300

    add unit tests for evm-tx bug

--- chains/evm/evmtransaction/evm-tx_test.go
+++ chains/evm/evmtransaction/evm-tx_test.go
@@ -1,1 +1,66 @@
 package evmtransaction
+
+import (
+	"math/big"
+	"testing"
+
+	mock_evmgaspricer "github.com/ChainSafe/chainbridge-core/chains/evm/evmgaspricer/mock"
+	"github.com/ChainSafe/chainbridge-core/e2e/evm"
+	"github.com/ethereum/go-ethereum/core/types"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/evmgaspricer"
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/suite"
+)
+
+type EVMTxTestSuite struct {
+	suite.Suite
+	client *mock_evmgaspricer.MockLondonGasClient
+}
+
+func TestRunTestSuite(t *testing.T) {
+	suite.Run(t, new(EVMTxTestSuite))
+}
+
+func (s *EVMTxTestSuite) SetupSuite()    {}
+func (s *EVMTxTestSuite) TearDownSuite() {}
+func (s *EVMTxTestSuite) SetupTest() {
+	gomockController := gomock.NewController(s.T())
+	s.client = mock_evmgaspricer.NewMockLondonGasClient(gomockController)
+}
+func (s *EVMTxTestSuite) TearDownTest() {}
+
+func (s *EVMTxTestSuite) TestNewTransactionWithStaticGasPricer() {
+	s.client.EXPECT().SuggestGasPrice(gomock.Any()).Return(big.NewInt(1000), nil)
+	txFabrid := NewTransaction
+	gasPriceClient := evmgaspricer.NewStaticGasPriceDeterminant(s.client, nil)
+	gp, err := gasPriceClient.GasPrice()
+	s.Nil(err)
+	tx, err := txFabrid(1, &common.Address{}, big.NewInt(0), 10000, gp, []byte{})
+	s.Nil(err)
+	rawTx, err := tx.RawWithSignature(evm.AliceKp.PrivateKey(), big.NewInt(420))
+	s.Nil(err)
+	txt := types.Transaction{}
+	err = txt.UnmarshalBinary(rawTx)
+	s.Nil(err)
+	s.Equal(types.LegacyTxType, int(txt.Type()))
+}
+
+func (s *EVMTxTestSuite) TestNewTransactionWithLondonGasPricer() {
+	s.client.EXPECT().BaseFee().Return(big.NewInt(1000), nil)
+	s.client.EXPECT().SuggestGasTipCap(gomock.Any()).Return(big.NewInt(1000), nil)
+	txFabrid := NewTransaction
+	gasPriceClient := evmgaspricer.NewLondonGasPriceClient(s.client, nil)
+	gp, err := gasPriceClient.GasPrice()
+	s.Nil(err)
+	tx, err := txFabrid(1, &common.Address{}, big.NewInt(0), 10000, gp, []byte{})
+	s.Nil(err)
+	rawTx, err := tx.RawWithSignature(evm.AliceKp.PrivateKey(), big.NewInt(420))
+	s.Nil(err)
+	txt := types.Transaction{}
+	err = txt.UnmarshalBinary(rawTx)
+	s.Nil(err)
+	s.Equal(types.DynamicFeeTxType, int(txt.Type()))
+}
