4359ad25c54bb0a7a04629e043a684253452786a
Matija Petrunic <matija.petrunic@fer.hr>
2021-11-12 12:53:40 +0100

    Fix evm-evm test name

--- e2e/evm-evm/evm-evm_test.go
+++ e2e/evm-evm/evm-evm_test.go
@@ -9,6 +9,9 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+const ETHEndpoint1 = "ws://localhost:8546"
+const ETHEndpoint2 = "ws://localhost:8548"
+
 // Alice key is used by the relayer, Eve key is used as admin and depositter
 func TestRunE2ETests(t *testing.T) {
 	suite.Run(t, evm.SetupEVM2EVMTestSuite(evmtransaction.NewTransaction, evmtransaction.NewTransaction, ETHEndpoint1, ETHEndpoint2, local.EveKp))
