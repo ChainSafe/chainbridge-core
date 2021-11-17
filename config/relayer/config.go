2bb24f9061a8dc2595b0f484b95c2597d83f0241
Matija Petrunic <matija.petrunic@fer.hr>
2021-11-17 17:34:15 +0100

    Remove Prometheus related flags

--- config/relayer/config.go
+++ config/relayer/config.go
@@ -1,33 +1,19 @@
 package relayer
 
 import (
-	"fmt"
-
 	"github.com/spf13/viper"
 )
 
 type RelayerConfig struct {
-	PrometheusPath            string `mapstructure:"PrometheusPath"`
-	PrometheusPort            uint64 `mapstructure:"PrometheusPort"`
 	OpenTelemetryCollectorURL string `mapstructure:"OpenTelemetryCollectorURL"`
 }
 
 func (c *RelayerConfig) Validate() error {
-	if c.PrometheusPort < 1 || c.PrometheusPort > 65535 {
-		return fmt.Errorf(`PrometheusPort outside of valid range of 1 - 65535`)
-	}
-
 	return nil
-}
-
-func setDefaultValues() {
-	viper.SetDefault("PrometheusPath", "/metrics")
-	viper.SetDefault("PrometheusPort", 2112)
 }
 
 func GetRelayerConfig(path string) (RelayerConfig, error) {
 	config := RelayerConfig{}
-	setDefaultValues()
 
 	viper.SetConfigFile(path)
 	err := viper.ReadInConfig()
