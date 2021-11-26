01c461c566c6e149129ef7816091985c298d815d
Matija Petrunic <matija.petrunic@fer.hr>
2021-11-26 12:50:45 +0100

    Update evm config godoc

--- config/chain/evm.go
+++ config/chain/evm.go
@@ -44,7 +44,8 @@ func (c *RawEVMConfig) Validate() error {
 	return nil
 }
 
-// NewEVMConfig creates and validates an instance of an EVMConfig
+// NewEVMConfig decodes and validates an instance of an EVMConfig from
+// raw chain config
 func NewEVMConfig(chainConfig map[string]interface{}) (*EVMConfig, error) {
 	var c RawEVMConfig
 	err := mapstructure.Decode(chainConfig, &c)
