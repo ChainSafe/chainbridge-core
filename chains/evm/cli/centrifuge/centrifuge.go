ee189dce9aa8fff43d3a6e25545b322ed71d209e
waymobetta <jbroethke@gmail.com>
2021-12-04 13:58:57 -0800

    update short/long description

--- chains/evm/cli/centrifuge/centrifuge.go
+++ chains/evm/cli/centrifuge/centrifuge.go
@@ -6,8 +6,8 @@ import (
 
 var CentrifugeCmd = &cobra.Command{
 	Use:   "centrifuge",
-	Short: "Set of commands for interaction with cetrifuge asset store contract",
-	Long:  "Set of commands for interaction with cetrifuge asset store contract",
+	Short: "Set of commands for interacting with a cetrifuge asset store contract",
+	Long:  "Set of commands for interacting with a cetrifuge asset store contract",
 }
 
 func init() {
