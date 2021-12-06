f46b333abaa012261da309bf9cab9957bcf2f7ff
Mak Muftic <mak@chainsafe.io>
2021-12-06 15:04:15 +0100

    Add missing files

---
+++ chains/evm/calls/contracts/erc20/erc20Handler.go
@@ -0,0 +1,26 @@
+package erc20
+
+import (
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/client"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/consts"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/contracts"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/transactor"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"strings"
+)
+
+type ERC20HandlerContract struct {
+	contracts.Contract
+}
+
+func NewERC20HandlerContract(
+	client client.ContractCallerDispatcherClient,
+	erc20HandlerContractAddress common.Address,
+	t transactor.Transactor,
+) *ERC20HandlerContract {
+	// load ABI
+	a, _ := abi.JSON(strings.NewReader(consts.ERC20HandlerABI))
+	b := common.FromHex(consts.ERC20HandlerBin)
+	return &ERC20HandlerContract{contracts.NewContract(erc20HandlerContractAddress, a, b, client, t)}
+}
