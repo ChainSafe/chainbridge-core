20495fa30c97311c8572ec30669e27763af74a17
Matija Petrunic <matija.petrunic@fer.hr>
2021-12-07 13:39:04 +0100

    Add godoc

--- logger/logger.go
+++ logger/logger.go
@@ -7,6 +7,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// ConfigureLogger configures logger level and assigns an array
+// of writers for logger to write to
 func ConfigureLogger(l zerolog.Level, writers ...io.Writer) {
 	zerolog.SetGlobalLevel(l)
 	mw := io.MultiWriter(writers...)
