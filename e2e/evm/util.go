24e7de08caf6f56e3d60fb907d9e37467df41f39
tcar <tcar121293@gmail.com>
2021-12-16 12:02:20 +0100

    move helper function to a sepparate file;
    Signed-off-by: tcar <tcar121293@gmail.com>

---
+++ e2e/evm/util.go
@@ -0,0 +1,66 @@
+package evm
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/consts"
+	"github.com/ChainSafe/chainbridge-core/util"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/rs/zerolog/log"
+)
+
+func WaitForProposalExecuted(client TestClient, bridge common.Address) {
+	startBlock, _ := client.LatestBlock()
+
+	query := ethereum.FilterQuery{
+		FromBlock: startBlock,
+		Addresses: []common.Address{bridge},
+		Topics: [][]common.Hash{
+			{util.ProposalEvent.GetTopic()},
+		},
+	}
+	ch := make(chan types.Log)
+
+	sub, err := client.SubscribeFilterLogs(context.Background(), query, ch)
+	if err != nil {
+		panic(err)
+	}
+	defer sub.Unsubscribe()
+
+	a, err := abi.JSON(strings.NewReader(consts.BridgeABI))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("dosassss\nnn")
+	timeout := time.After(TestTimeout)
+	for {
+		select {
+		case evt := <-ch:
+			fmt.Println("nesjedosloooooo\nnn")
+			out, err := a.Unpack("ProposalEvent", evt.Data)
+			if err != nil {
+				panic(err)
+			}
+			status := abi.ConvertType(out[2], new(uint8)).(*uint8)
+			// Check status
+			if util.IsExecuted(*status) {
+				log.Info().Msgf("Got Proposal executed event status, continuing..., status: %v", *status)
+				return
+			} else {
+				log.Info().Msgf("Got Proposal event status: %v", *status)
+			}
+		case err := <-sub.Err():
+			if err != nil {
+				panic(err)
+			}
+		case <-timeout:
+			panic("Test timed out waiting for ProposalCreated event")
+		}
+	}
+}
