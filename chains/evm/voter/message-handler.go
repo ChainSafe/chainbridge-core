b99dabbc8cf866d46d8cb8c712bc765369674251
tcar121293 <tcar121293@gmail.com>
2022-02-01 10:57:19 +0100

    Update message handler to handle the priority data (#244)
    * update message handlers with priority data;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * update payload len check;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * add tests for message handlers;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * handle priority data without breaking changes;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * use metadata in message handler;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * fix tests;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * remove unused var;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * fix lint errors;
    
    Signed-off-by: tcar <tcar121293@gmail.com>
    
    * Remove extra spaces
    
    * Lint
    
    Co-authored-by: Matija Petrunic <matija.petrunic@fer.hr>

--- chains/evm/voter/message-handler.go
+++ chains/evm/voter/message-handler.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"math/big"
+
 	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/contracts/bridge"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/voter/proposal"
 	"github.com/ChainSafe/chainbridge-core/relayer/message"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/rs/zerolog/log"
-	"math/big"
 )
 
 type MessageHandlerFunc func(m *message.Message, handlerAddr, bridgeAddress common.Address) (*proposal.Proposal, error)
@@ -75,35 +76,36 @@ func ERC20MessageHandler(m *message.Message, handlerAddr, bridgeAddress common.A
 	}
 	amount, ok := m.Payload[0].([]byte)
 	if !ok {
-		return nil, errors.New("wrong payloads amount format")
+		return nil, errors.New("wrong payload amount format")
 	}
 	recipient, ok := m.Payload[1].([]byte)
 	if !ok {
-		return nil, errors.New("wrong payloads recipient format")
+		return nil, errors.New("wrong payload recipient format")
 	}
 	var data []byte
 	data = append(data, common.LeftPadBytes(amount, 32)...) // amount (uint256)
 	recipientLen := big.NewInt(int64(len(recipient))).Bytes()
 	data = append(data, common.LeftPadBytes(recipientLen, 32)...) // length of recipient (uint256)
 	data = append(data, recipient...)                             // recipient ([]byte)
-	return proposal.NewProposal(m.Source, m.DepositNonce, m.ResourceId, data, handlerAddr, bridgeAddress), nil
+	return proposal.NewProposal(m.Source, m.DepositNonce, m.ResourceId, data, handlerAddr, bridgeAddress, m.Metadata), nil
 }
 
 func ERC721MessageHandler(msg *message.Message, handlerAddr, bridgeAddress common.Address) (*proposal.Proposal, error) {
+
 	if len(msg.Payload) != 3 {
 		return nil, errors.New("malformed payload. Len  of payload should be 3")
 	}
 	tokenID, ok := msg.Payload[0].([]byte)
 	if !ok {
-		return nil, errors.New("wrong payloads tokenID format")
+		return nil, errors.New("wrong payload tokenID format")
 	}
 	recipient, ok := msg.Payload[1].([]byte)
 	if !ok {
-		return nil, errors.New("wrong payloads recipient format")
+		return nil, errors.New("wrong payload recipient format")
 	}
 	metadata, ok := msg.Payload[2].([]byte)
 	if !ok {
-		return nil, errors.New("wrong payloads metadata format")
+		return nil, errors.New("wrong payload metadata format")
 	}
 	data := bytes.Buffer{}
 	data.Write(common.LeftPadBytes(tokenID, 32))
@@ -113,7 +115,7 @@ func ERC721MessageHandler(msg *message.Message, handlerAddr, bridgeAddress commo
 	metadataLen := big.NewInt(int64(len(metadata))).Bytes()
 	data.Write(common.LeftPadBytes(metadataLen, 32))
 	data.Write(metadata)
-	return proposal.NewProposal(msg.Source, msg.DepositNonce, msg.ResourceId, data.Bytes(), handlerAddr, bridgeAddress), nil
+	return proposal.NewProposal(msg.Source, msg.DepositNonce, msg.ResourceId, data.Bytes(), handlerAddr, bridgeAddress, msg.Metadata), nil
 }
 
 func GenericMessageHandler(msg *message.Message, handlerAddr, bridgeAddress common.Address) (*proposal.Proposal, error) {
@@ -122,11 +124,11 @@ func GenericMessageHandler(msg *message.Message, handlerAddr, bridgeAddress comm
 	}
 	metadata, ok := msg.Payload[0].([]byte)
 	if !ok {
-		return nil, errors.New("unable to convert metadata to []byte")
+		return nil, errors.New("wrong payload metadata format")
 	}
 	data := bytes.Buffer{}
 	metadataLen := big.NewInt(int64(len(metadata))).Bytes()
 	data.Write(common.LeftPadBytes(metadataLen, 32)) // length of metadata (uint256)
 	data.Write(metadata)
-	return proposal.NewProposal(msg.Source, msg.DepositNonce, msg.ResourceId, data.Bytes(), handlerAddr, bridgeAddress), nil
+	return proposal.NewProposal(msg.Source, msg.DepositNonce, msg.ResourceId, data.Bytes(), handlerAddr, bridgeAddress, msg.Metadata), nil
 }
