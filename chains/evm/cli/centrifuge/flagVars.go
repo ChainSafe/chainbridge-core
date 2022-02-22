4d3b6bf5165940cc0b96ea03046cb968af999e65
vezenovm <mvezenov@gmail.com>
2022-02-22 00:46:53 -0500

    added cli command for deposit, and changed method of decoding deposit and execute function signatures for registering a generic resource

--- chains/evm/cli/centrifuge/flagVars.go
+++ chains/evm/cli/centrifuge/flagVars.go
@@ -4,25 +4,37 @@ import (
 	"math/big"
 
 	"github.com/ChainSafe/chainbridge-core/crypto/secp256k1"
+	"github.com/ChainSafe/chainbridge-core/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 //flag vars
 var (
-	Hash    string
-	Address string
+	Hash       string
+	Address    string
+	Metadata   string
+	Recipient  string
+	Bridge     string
+	DomainID   uint8
+	ResourceID string
+	Priority   string
 )
 
 //processed flag vars
 var (
-	StoreAddr common.Address
-	ByteHash  [32]byte
+	StoreAddr          common.Address
+	ByteHash           [32]byte
+	MetadataBytes      []byte
+	RecipientAddr      common.Address
+	BridgeAddr         common.Address
+	ResourceIdBytesArr types.ResourceID
 )
 
 // global flags
 var (
 	url           string
 	gasPrice      *big.Int
+	gasLimit      uint64
 	senderKeyPair *secp256k1.Keypair
 	prepare       bool
 )
